e3ab186fa319d24103a3eb56f9f2b9874e7c7161
Connor Rogers <crogers@pivotal.io>
2019-03-11 17:10:19 +0000

    WIP
    [#164338662]
    
    Co-authored-by: George Blue <gblue@pivotal.io>

--- src/rabbitmq-service-broker/binding/binding.go
+++ src/rabbitmq-service-broker/binding/binding.go
@@ -1,51 +1,73 @@
 package binding
 
 import (
+	"encoding/json"
 	"fmt"
-
-	"github.com/mitchellh/mapstructure"
 )
 
+type protocol struct {
+	Username  string   `json:"username"`
+	Password  string   `json:"password"`
+	VHost     string   `json:"vhost,omitempty"`
+	Hostname  string   `json:"host"`
+	Hostnames []string `json:"hosts"`
+	URI       string   `json:"uri"`
+	URIs      []string `json:"uris"`
+	Port      int      `json:"port"`
+	TLS       bool     `json:"ssl"`
+	Path      string   `json:"path,omitempty"`
+}
+
+type protocols map[string]protocol
+
 type binding struct {
-	DashboardURL string `mapstructure:"dashboard_url"`
-	// Username     string   `mapstructure:"username"`
-	Password    string   `mapstructure:"password"`
-	Hostname    string   `mapstructure:"hostname"`
-	Hostnames   []string `mapstructure:"hostnames"`
-	HTTPAPIURI  string   `mapstructure:"http_api_uri"`
-	HTTPAPIURIs []string `mapstructure:"http_api_uris"`
-	URI         string   `mapstructure:"uri"`
-	// URIs         []string `mapstructure:"uris"`
-	// VHOST        string   `mapstructure:"vhost"`
-	TLS bool `mapstructure:"ssl"`
-	// Protocols    map[string]map[string]interface{} `mapstructure:"protocols"`
+	DashboardURL string    `json:"dashboard_url"`
+	Username     string    `json:"username"`
+	Password     string    `json:"password"`
+	Hostname     string    `json:"hostname"`
+	Hostnames    []string  `json:"hostnames"`
+	HTTPAPIURI   string    `json:"http_api_uri"`
+	HTTPAPIURIs  []string  `json:"http_api_uris"`
+	URI          string    `json:"uri"`
+	URIs         []string  `json:"uris"`
+	VHost        string    `json:"vhost"`
+	TLS          bool      `json:"ssl"`
+	Protocols    protocols `json:"protocols"`
 }
 
 type Builder struct {
-	MgmtDomain string
-	Hostnames  []string
-	Vhost      string
-	Username   string
-	Password   string
-	AMQPPort   string
-	MQTTPort   string
-	STOMPPort  string
-	TLS        bool
+	MgmtDomain    string
+	Hostnames     []string
+	VHost         string
+	Username      string
+	Password      string
+	TLS           bool
+	ProtocolPorts map[string]int // key=protcol, value=port, e.g. "amqp": 4567
 }
 
-func (b Builder) AsMap() (output map[string]interface{}) {
+func (b Builder) AsMap() (output map[string]interface{}, err error) {
 	bind := binding{
+		VHost:        b.VHost,
+		Username:     b.Username,
+		Password:     b.Password,
 		DashboardURL: b.dashboardURL(),
 		Hostname:     b.firstHostname(),
 		Hostnames:    b.Hostnames,
 		HTTPAPIURI:   b.httpapiuri(),
 		HTTPAPIURIs:  b.httpapiuris(),
-		Password:     b.Password,
+		URI:          b.uri(b.firstHostname()),
+		URIs:         b.uris(),
 		TLS:          b.TLS,
-		URI:          b.uri(),
+		Protocols:    b.protocols(),
 	}
-	mapstructure.Decode(bind, &output)
-	return output
+
+	bytes, err := json.Marshal(bind)
+	if err != nil {
+		return output, err
+	}
+
+	err = json.Unmarshal(bytes, &output)
+	return output, err
 }
 
 func (b Builder) dashboardURL() string {
@@ -56,12 +78,40 @@ func (b Builder) firstHostname() string {
 	return b.Hostnames[0]
 }
 
-// {:uri           (uri-for (cfg/amqp-scheme) (URLEncoder/encode username) password first-node-host vhost)
-//  :uris          (map #(uri-for (cfg/amqp-scheme) (URLEncoder/encode username) password % vhost) node-hosts)
-//   (format "%s://%s:%s@%s/%s" scheme username password node-host (URLEncoder/encode vhost)))
+func (b Builder) uri(hostname string) string {
+	return fmt.Sprintf("%s://%s:%s@%s/%s", b.amqpScheme(), b.Username, b.Password, hostname, b.VHost)
+}
 
-func (b Builder) uri() string {
-	return fmt.Sprintf("%s://%s:%s@%s/%s", b.amqpScheme(), b.Username, b.Password, b.firstHostname(), b.Vhost)
+func (b Builder) uriWithPort(hostname string, port int) string {
+	return fmt.Sprintf("%s://%s:%s@%s:%d/%s", b.amqpScheme(), b.Username, b.Password, hostname, port, b.VHost)
+}
+
+func (b Builder) uriWithSchemeAndPort(hostname string, port int, scheme string) string {
+	return fmt.Sprintf("%s://%s:%s@%s:%d/%s", scheme, b.Username, b.Password, hostname, port, b.VHost)
+}
+
+func (b Builder) uris() []string {
+	var uris []string
+	for _, hostname := range b.Hostnames {
+		uris = append(uris, b.uri(hostname))
+	}
+	return uris
+}
+
+func (b Builder) urisWithPort(port int) []string {
+	var uris []string
+	for _, hostname := range b.Hostnames {
+		uris = append(uris, b.uriWithPort(hostname, port))
+	}
+	return uris
+}
+
+func (b Builder) urisWithSchemeAndPort(port int, scheme string) []string {
+	var uris []string
+	for _, hostname := range b.Hostnames {
+		uris = append(uris, b.uriWithSchemeAndPort(hostname, port, scheme))
+	}
+	return uris
 }
 
 func (b Builder) amqpScheme() string {
@@ -77,4 +127,47 @@ func (b Builder) httpapiuri() string {
 
 func (b Builder) httpapiuris() []string {
 	return []string{b.httpapiuri()}
+}
+
+func (b Builder) protocols() protocols {
+	ps := make(protocols)
+	for protocol, port := range b.ProtocolPorts {
+		switch protocol {
+		case "amqp":
+			ps["amqp"] = b.addAMQPProtocol(port)
+		case "amqp/ssl":
+			ps["amqp+ssl"] = b.addAMQPProtocol(port)
+		}
+	}
+	ps["management"] = b.addMgmtProtocol()
+	return ps
+}
+
+func (b Builder) addMgmtProtocol() protocol {
+	return protocol{
+		Username: b.Username,
+		Password: b.Password,
+		// VHost:     b.VHost,
+		Hostname:  b.firstHostname(),
+		Hostnames: b.Hostnames,
+		URI:       b.uriWithSchemeAndPort(b.firstHostname(), 15672, "http"),
+		URIs:      b.urisWithSchemeAndPort(15672, "http"),
+		Port:      15672,
+		TLS:       false,
+		Path:      "/api/",
+	}
+}
+
+func (b Builder) addAMQPProtocol(port int) protocol {
+	return protocol{
+		Username:  b.Username,
+		Password:  b.Password,
+		VHost:     b.VHost,
+		Hostname:  b.firstHostname(),
+		Hostnames: b.Hostnames,
+		URI:       b.uriWithPort(b.firstHostname(), port),
+		URIs:      b.urisWithPort(port),
+		Port:      port,
+		TLS:       b.TLS,
+	}
 }
